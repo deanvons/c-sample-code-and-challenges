b4b6f414e71a20f8efe3566593626a0a203ab26e
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-08-26 14:15:45 +0200

    FEAT: added challenges for 1.1 and 1.2

---
+++ Challenges/1.1_Challenge_SpaceshipThrust/spaceship_thrust.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * === Spaceship Thrust === 
+ *
+ * We are working on the man-machine interface of a revolutionary spaceship.
+ * Define enumeration constants for different predefined thrust levels, these
+ * levels will be used to communicate with the engine controller.
+ * 
+ * The thrust levels are:
+ *  - none:     0
+ *  - low:      5
+ *  - medium:   9
+ *  - high:     12
+ *  - maximum:  20
+ * 
+ * Then launch the rocket!
+ * We now want to log the thrust level in some critical points:
+ *  - set thrust to none ---> log ready to go level
+ *  - set thrust to maximum ---> log take off level
+ *  - set thrust to medium ---> log entry into the ionosphere
+ *  - set thrust to low ---> log traveling to deep space
+ * 
+ */
+
+
+int main()
+{
+ 
+    return EXIT_SUCCESS;
+}
