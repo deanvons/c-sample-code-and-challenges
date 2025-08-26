c8ae929cab7085228baacecd8fd7c8da6a01111c
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-08-26 09:23:54 +0200

    FEAT: added sample code for lesson 1.1 and challenge

---
+++ Sample Code/1.1_Characters/example_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    char c = 'A';
+    printf("c = '%c'\n", c);
+    printf("is lower case:  %d\n", islower(c));
+    printf("is upper case:  %d\n", isupper(c));
+    printf("is digit        %d\n", isdigit(c));
+    printf("\n");
+
+    c = tolower(c);
+    printf("to lower case:  '%c'\n", c);
+    printf("\n");
+    
+    c = '0';
+    printf("c = '%c'\n", c);
+    printf("is digit        %d\n", isdigit(c));
+
+    return EXIT_SUCCESS;
+}
